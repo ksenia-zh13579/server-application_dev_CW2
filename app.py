3a0691aa60c8d3637e1083b0c3d09762f2a6c667
Ksenia Zhuzhleva <zhuzhleva.k.a@edu.mirea.ru>
2026-03-21 15:06:05 +0300

    complete task 3.2

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from models import UserCreate
+from products_data import sample_products
 
 app = FastAPI()
 
-@app.post('/create_user/')
+# task 3.1
+@app.post('/create_user')
 def create_user(user : UserCreate):
     return user
+
+# task 3.2
+@app.get('/product/{product_id}')
+def get_product_by_id(product_id : int):
+    for product in sample_products:
+        if product['product_id'] == product_id:
+            return product
+
+@app.get('/products/search')
+def search_products(keyword : str, category : str = Query(default=None), limit : int = Query(default=10, ge=0)):
+    res = []
+    for product in sample_products:
+        if len(res) < limit and keyword in product['name'] and (not category or product['category'] == category):
+            res.append(product)
+    return res
